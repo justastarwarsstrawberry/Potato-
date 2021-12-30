f52989d4702195845aa8af2614efa07639ad3826
Something-AI <63701816+Something-AI@users.noreply.github.com>
2021-12-30 17:51:39 +0200

    update, rebuild

--- start-up.py
+++ start-up.py
@@ -2,25 +2,35 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
-def loadUi():
-    label = QtWidgets.QLabel(win)
-    label.setObjectName("label")
-    label.setText("Welcome to the mod maker for RW!")
-    label.setGeometry(int(win.width()/2),int(win.height()/2),100,100)
-    filemenu = win.menuBar().addMenu('file')
-    filemenu.open = filemenu.addMenu('open')
-    exit = QtWidgets.QAction('exit app',win)
-    exit.setShortcut('Ctrl+Q')
-    exit.triggered.connect(lambda: QApplication.quit())
-    filemenu.addAction(exit)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.resize(800, 600)
+        self.setWindowTitle("RW modmaker!")
+        self.setCentralWidget(QtWidgets.QWidget(self))
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setObjectName("label")
+        self.label.setText("Welcome to the mod maker for RW!")
+        self.label.setGeometry(int(self.width()/2),int(self.height()/2),100,100)
+        self.label.adjustSize()
+
+        self.filemenu = self.menuBar().addMenu("File")
+        self.filemenu.open = self.filemenu.addAction('Open project..')
+        self.filemenu.new = self.filemenu.addAction('New project..')
+
+        #filemenu.open.triggered.connect(lambda: })
+        self.exit = QtWidgets.QAction('exit app',self)
+        self.exit.setShortcut("Ctrl+Q")
+        self.exit.triggered.connect(lambda: QApplication.quit())
+        self.filemenu.addAction(self.exit)
 
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.resize(800, 600)
-    win.setWindowTitle("RW modmaker!")
-    win.setCentralWidget(QtWidgets.QWidget(win))
-    loadUi()
+    win = MainWindow()
+    win.initUI()
     win.show()
     sys.exit(app.exec_())
